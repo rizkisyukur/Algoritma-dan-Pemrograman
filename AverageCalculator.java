1b2e73018494bdf750eb46c4c9269e07d390dfc6
rizkisyukur <145557850+rizkisyukur@users.noreply.github.com>
2024-07-14 11:05:44 +0700

    Update AverageCalculator.java

--- AverageCalculator.java
+++ AverageCalculator.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class AverageCalculator {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan jumlah elemen dalam array: ");
+        int n = scanner.nextInt();
+        double[] array = new double[n];
+
+        System.out.println("Masukkan elemen-elemen array:");
+        for (int i = 0; i < n; ++i) {
+            array[i] = scanner.nextDouble();
+        }
+
+        double sum = 0;
+        for (double num : array) {
+            sum += num;
+        }
+
+        double average = sum / n;
+        System.out.println("Nilai rata-rata: " + average);
+    }
 }
