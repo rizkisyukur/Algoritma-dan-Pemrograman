82579ecdf3abad88f92955e8fdb41a817107d64b
rizkisyukur <145557850+rizkisyukur@users.noreply.github.com>
2024-07-14 11:06:16 +0700

    Update SpeedCalculator.java

--- SpeedCalculator.java
+++ SpeedCalculator.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class SpeedCalculator {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan jarak (dalam kilometer): ");
+        double distance = scanner.nextDouble();
+        System.out.print("Masukkan waktu tempuh (dalam jam): ");
+        double time = scanner.nextDouble();
+
+        double speed = distance / time;
+
+        System.out.println("Kecepatan: " + speed + " km/jam");
+    }
 }
